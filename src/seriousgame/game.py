906733c4d7d8a043a8036c3f53ad13d4bfaf17d3
ThT12 <ThT12@ThT12.fr>
2017-11-16 21:47:47 +0100

    add missing headers

--- src/seriousgame/game.py
+++ src/seriousgame/game.py
@@ -5,13 +5,21 @@ from seriousgame.player import Player
 class Game:
 
     def __init__(self, player=Player(), country=Country()):
+        """ Constructor
+
+        Args:
+            player (Player): player information
+            country (Country): country information
+        """
         self.player = player
         self.country = country
 
     def new_turn(self):
+        """ Makes a new turn in the game"""
         self.player.new_turn()
         self.country.new_turn()
 
     def play(self):
+        """ Launch the game until it is finish"""
         while not self.country.is_win() and not self.country.is_lost():
             self.new_turn()
